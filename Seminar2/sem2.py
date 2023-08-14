4d460724fb076bf98c0dfcb3129ff8179daa4b12
kate <01redcat@mail.ru>
2023-08-15 00:35:29 +0300

    sem 2 формы

--- Seminar2/sem2.py
+++ Seminar2/sem2.py
@@ -30,13 +30,36 @@
 # результата выбранной операции и переход на страницу с
 # результатом.
 
+# Задание №6
+# Создать страницу, на которой будет форма для ввода имени
+# и возраста пользователя и кнопка "Отправить"
+# При нажатии на кнопку будет произведена проверка
+# возраста и переход на страницу с результатом или на
+# страницу с ошибкой в случае некорректного возраста.
 
+# Задание №7
+# Создать страницу, на которой будет форма для ввода числа
+# и кнопка "Отправить"
+# При нажатии на кнопку будет произведено
+# перенаправление на страницу с результатом, где будет
+# выведено введенное число и его квадрат.
+
+# Задание №8
+# Создать страницу, на которой будет форма для ввода имени
+# и кнопка "Отправить"
+# При нажатии на кнопку будет произведено
+# перенаправление на страницу с flash сообщением, где будет
+# выведено "Привет, {имя}!"
+
+
+import logging
 from pathlib import PurePath, Path
-from flask import Flask, redirect, render_template, request, flash, url_for
+from flask import Flask, redirect, render_template, request, flash, url_for, abort
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = b'5f214cacbd30c2ae4784b520f17912ae0d5d8c16ae98128e3f549546221265e4'
+logger = logging.getLogger(__name__)
 
 
 @app.route('/')
@@ -82,7 +105,7 @@ def login():
 def enter():
     if request.method == 'POST':
         text = request.form.get('text')
-        res = len(text.split(' '))
+        res = len(text.split())
         if not request.form['text']:
             flash('Введите текст!', 'danger')
             return redirect(url_for('enter'))
@@ -110,6 +133,48 @@ def calc():
         return f'{res}'
     
     return render_template('calc.html')
+
+
+@app.route('/user', methods=['GET', 'POST'])
+def user_data():
+    if request.method == 'POST':
+        #user_name = request.form.get('user_name')
+        age = request.form.get('age')
+        if int(age) < 18:
+            abort(404)
+        flash('Форма успешно отправлена!', 'success')
+    return render_template('user_data.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    logger.warning(e)
+    return render_template('404.html'), 404           
+
+
+@app.route('/number', methods=['GET', 'POST'])
+def number():
+    if request.method == 'POST':
+        number = request.form.get('number')
+        res = int(number) * int(number) 
+        return redirect(url_for('result', result=res))
+    return render_template('number.html')
+
+
+@app.route('/result')
+def result():
+    return request.args.get('result')   
+
+
+@app.route('/hello_name', methods=['GET', 'POST'])
+def hello_name():
+    if request.method == 'POST':
+        user_name = request.form.get('name')
+        if not request.form['name']:
+            flash('Введите имя!', 'danger')
+            return redirect(url_for('hello_name'))
+        flash(f'Привет, {user_name}!', 'success')
+    return render_template('hello_name.html')
 
 
 if __name__ == '__main__':
